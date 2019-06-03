33b7ccd8344b40af695ea9d06fae848a5eb6d0ad
Paul <paul.retail@me.com>
2019-06-03 18:14:07 +0200

    import sans la progressbar

--- Bacchus/view controller/ImportForm.cs
+++ Bacchus/view controller/ImportForm.cs
@@ -42,61 +42,143 @@ namespace Bacchus
 
         private void OverwriteButton_Click(object sender, EventArgs e)
         {
-            //clear db
+
+            // test les erreurs avec le fichier CSV
             try
             {
-                using (var con = new SQLiteConnection("Data Source = Bacchus.SQLite ;Version=3;New=False;Compress=True;"))
+                using (var reader = new StreamReader(@CSVNameTextBox.Text))
                 {
-                    //delete all statement of table Articles
-                    using (var Command = new SQLiteCommand("DELETE FROM Articles;"))
+                    // nettoie la bd
+                    try
                     {
-                        //execute query
-                        Command.Connection = con;
-                        Command.Connection.Open();
-                        Command.ExecuteNonQuery();
-                        con.Close();
+                        using (var con = new SQLiteConnection("Data Source = Bacchus.SQLite ;Version=3;New=False;Compress=True;"))
+                        {
+                            // supprime le contenu de la table articles
+                            using (var Command = new SQLiteCommand("DELETE FROM Articles;"))
+                            {
+                                //execute query
+                                Command.Connection = con;
+                                Command.Connection.Open();
+                                Command.ExecuteNonQuery();
+                                con.Close();
+                            }
+
+                            // supprime le contenu de la table marques
+                            using (var Command = new SQLiteCommand("DELETE FROM Marques;"))
+                            {
+                                //execute query
+                                Command.Connection = con;
+                                Command.Connection.Open();
+                                Command.ExecuteNonQuery();
+                                con.Close();
+                            }
+
+                            // supprime le contenu de la table sousfamilles
+                            using (var Command = new SQLiteCommand("DELETE FROM SousFamilles;"))
+                            {
+                                //execute query
+                                Command.Connection = con;
+                                Command.Connection.Open();
+                                Command.ExecuteNonQuery();
+                                con.Close();
+                            }
+
+                            // supprime le contenu de la table familles
+                            using (var Command = new SQLiteCommand("DELETE FROM Familles;"))
+                            {
+                                //execute query
+                                Command.Connection = con;
+                                Command.Connection.Open();
+                                Command.ExecuteNonQuery();
+                                con.Close();
+                            }
+                        }
+
+                    }
+                    catch (Exception ExceptionCaught)
+                    {
+                        MessageBox.Show("Error while clearing db : " + ExceptionCaught);
                     }
 
-                    //delete all statement of table Marques
-                    using (var Command = new SQLiteCommand("DELETE FROM Marques;"))
+                
+                    bool TitleLine = true;
+                    while (!reader.EndOfStream)
                     {
-                        //execute query
-                        Command.Connection = con;
-                        Command.Connection.Open();
-                        Command.ExecuteNonQuery();
-                        con.Close();
-                    }
+                        var line = reader.ReadLine();
+                        var values = line.Split(';');
 
-                    //delete all statement of table SousFamilles
-                    using (var Command = new SQLiteCommand("DELETE FROM SousFamilles;"))
-                    {
-                        //execute query
-                        Command.Connection = con;
-                        Command.Connection.Open();
-                        Command.ExecuteNonQuery();
-                        con.Close();
-                    }
+                        // verification qui permet d'éviter la ligne titre
+                        if (TitleLine == false)
+                        {
+                            // verifie si la reference est valide 
+                            if (ArticleDAO.VerifArticleRef(values[1]) == true)
+                            {
 
-                    //delete all statement of table Familles
-                    using (var Command = new SQLiteCommand("DELETE FROM Familles;"))
-                    {
-                        //execute query
-                        Command.Connection = con;
-                        Command.Connection.Open();
-                        Command.ExecuteNonQuery();
-                        con.Close();
-                    }
+                                // cherche la quantité, si elle existe, dans la premiere colonne
+                                var firstSpaceIndex = values[0].IndexOf(" ");
+                                var Quantity = values[0].Substring(0, firstSpaceIndex); // la quantite si elle existe
+                                var Description = values[0].Substring(firstSpaceIndex + 1); // la description si il y a un quantite
+                                int IntQuantity;
+                                if (int.TryParse(Quantity, out IntQuantity) == false)
+                                {
+                                    Description = values[0];
+                                    IntQuantity = 1;
+                                }
 
-                    MessageBox.Show("db Cleared");
+
+                                // si la famille existe la trouve sinon la creer 
+                                Family FamilyToAdd = FamilyDAO.GetFamilyByName(values[3]);
+                                if (FamilyToAdd == null)
+                                {
+                                    FamilyToAdd = new Family(values[3]);
+                                    FamilyDAO.AddFamily(FamilyToAdd);
+                                }
+
+                                // si la sous famille existe la trouve sinon la creer
+                                SubFamily SubFamilyToAdd = SubFamilyDAO.GetSubFamilyByName(values[4], FamilyToAdd);
+                                if (SubFamilyToAdd == null)
+                                {
+                                    SubFamilyToAdd = new SubFamily(values[4], FamilyToAdd);
+                                    SubFamilyDAO.AddSubFamily(SubFamilyToAdd);
+                                }
+
+                                // si la marque existe la trouve sinon la creer
+                                Brand BrandToAdd = BrandDAO.GetBrandByName(values[2]);
+                                if (BrandToAdd == null)
+                                {
+                                    BrandToAdd = new Brand(values[2]);
+                                    BrandDAO.AddBrand(BrandToAdd);
+                                }
+
+                                // vérifie si le prix est bien un double
+                                float FloatPrice;
+                                if (float.TryParse(values[5], out FloatPrice))
+                                {
+                                    // si la reference de l'article existe la trouve sinon creer l'article
+                                    Article ArticleToAdd = ArticleDAO.GetArticleById(values[1]);
+                                    if (ArticleToAdd == null)
+                                    {
+                                        //créer l'article et le rajoute à la bd
+                                        ArticleToAdd = new Article(values[1], Description, SubFamilyToAdd, BrandToAdd, FloatPrice, IntQuantity);
+                                        ArticleDAO.AddArticle(ArticleToAdd);
+                                    }
+
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            TitleLine = false;
+                        }
+                    }
+                    this.Close();
                 }
-
             }
-            catch (Exception ExceptionCaught)
+            catch (System.Exception Excep)
             {
-                MessageBox.Show("Error while clearing db : " + ExceptionCaught);
+                MessageBox.Show(Excep.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
-            //add to db
         }
 
         /// <summary>
@@ -106,6 +188,7 @@ namespace Bacchus
         /// <param name="Event"></param>
         private void AddButton_Click(object Sender, EventArgs Event)
         {
+            // test les erreurs avec le fichier CSV
             try
             {
                 using (var reader = new StreamReader(@CSVNameTextBox.Text))
@@ -159,7 +242,7 @@ namespace Bacchus
                                     BrandDAO.AddBrand(BrandToAdd);
                                 }
 
-                                // vérifie si le prix est bein un double
+                                // vérifie si le prix est bien un double
                                 float FloatPrice;
                                 if (float.TryParse(values[5], out FloatPrice))
                                 {
